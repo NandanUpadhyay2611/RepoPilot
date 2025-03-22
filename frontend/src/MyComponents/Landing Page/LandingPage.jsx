bd88d27c275a53c56a4789968c35309ddbf8bad3
NandanUpadhyay2611 <menotnandan6969@gmail.com>
2025-03-23 00:54:26 +0530

    landingpage css fix

--- frontend/src/MyComponents/Landing Page/LandingPage.jsx
+++ frontend/src/MyComponents/Landing Page/LandingPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './LandingPage.css';
+// import React from 'react';
+import './landingPage.css';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
